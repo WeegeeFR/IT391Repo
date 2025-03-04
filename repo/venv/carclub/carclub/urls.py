67ddd5163ca0b5c575b08f53335bfe082044a636
lf-24 <lfgonz3@ilstu.edu>
2025-03-04 12:56:29 -0600

    Made home templates

--- repo/venv/carclub/carclub/urls.py
+++ repo/venv/carclub/carclub/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("test/", include("backend.urls")),
+    # path("test/", include("backend.urls")),
     path('admin/', admin.site.urls),
+    path('', include('backend.urls')),
 ]
